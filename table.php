3d3d2e3c6ee763c8313f60411dea49973006fcf0
Normal Security <glenn.sorrentino@pm.me>
2021-04-12 22:36:26 -0700

    tables

---
+++ table.php
@@ -0,0 +1,35 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="author" content="Glenn Sorrentino">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Table - Simple Design System</title>
+	<link rel="stylesheet" href="css/style.css">
+
+	<script src="https://code.jquery.com/jquery-latest.min.js"></script>
+	<script src="https://glenn-sorrentino.github.io/design-system/builder/js/builder.js"></script>
+	<script src="https://glenn-sorrentino.github.io/design-system/js/main.js"></script>
+</head>
+
+<body>
+
+		<!-- HEADER -->
+
+		<?php include "components/header/html/header.html" ?>
+
+		<main id="content">
+
+			<?php include "components/table/html/table.html" ?>
+
+		</main>
+
+		<!-- FOOTER -->
+
+
+</body>
+
+</html>
