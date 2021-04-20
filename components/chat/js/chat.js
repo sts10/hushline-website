377ff4f0051a49b67dc18fbb0bb603a8ff925da5
Normal Security <glenn.sorrentino@pm.me>
2021-04-20 13:07:04 -0700

    Update chat.js

--- components/chat/js/chat.js
+++ components/chat/js/chat.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
   $('.chatWindow').scrollTop($('.chatWindow')[0].scrollHeight);
+
+  $('.usersTab').on('click', function() {
+    $('.pinnedList').addClass('pinnedListVisible');
+    $('.mainStage').addClass('hidden');
+    $('.usersTab').addClass('selected');
+    $('.chatTab').removeClass('selected');
+  });
+
+  $('.chatTab').on('click', function() {
+    $('.pinnedList').removeClass('pinnedListVisible');
+    $('.mainStage').removeClass('hidden');
+    $('.usersTab').removeClass('selected');
+    $('.chatTab').addClass('selected');
+  });
+
 });
