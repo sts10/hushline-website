40c28b93111c0a309717adae6a1a1ce3449adc46
Glenn <glenn.sorrentino@pm.me>
2021-05-16 13:17:46 -0700

    header js

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 	<link rel="stylesheet" href="css/style.css">
 
 	<script src="js/jquery-min.js"></script>
-	<script src="js/main.js"></script>
+	<script src="components/header/js/header.js"></script>
 	<script src="builder/js/builder.js"></script>
 	<script src="components/chat/js/chat.js"></script>
 	<script src="components/footer/js/copyright.js"></script>
