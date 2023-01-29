9daf4b9c4ccd894c1707489078395f29bd30263c
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-01-29 14:31:05 -0800

    add accordions

--- design-system/js/builder.js
+++ design-system/js/builder.js
@@ -10,4 +10,59 @@ $(document).ready(function() {
     $('.menuControlsBtn').toggleClass('flipped');
   });
 
+  $('.bannerHeader .expandBtn').on('click', function() {
+    $('.bannerSection').toggleClass('hidden');
+    $('.bannerHeader .expandBtn').toggleClass('flipped');
+  });
+
+  $('.hHeader .expandBtn').on('click', function() {
+    $('.headerSection').toggleClass('hidden');
+    $('.hHeader .expandBtn').toggleClass('flipped');
+  });
+
+  $('.introHeader .expandBtn').on('click', function() {
+    $('.introSection').toggleClass('hidden');
+    $('.introHeader .expandBtn').toggleClass('flipped');
+  });
+
+  $('.missionHeader .expandBtn').on('click', function() {
+    $('.missionSection').toggleClass('hidden');
+    $('.missionHeader .expandBtn').toggleClass('flipped');
+  });
+
+  $('.overviewHeader .expandBtn').on('click', function() {
+    $('.overviewSection').toggleClass('hidden');
+    $('.overviewHeader .expandBtn').toggleClass('flipped');
+  });
+
+  $('.featureHeader .expandBtn').on('click', function() {
+    $('.featureRichSection').toggleClass('hidden');
+    $('.featureHeader .expandBtn').toggleClass('flipped');
+  });
+
+  $('.cardHeader .expandBtn').on('click', function() {
+    $('.cardSection').toggleClass('hidden');
+    $('.cardHeader .expandBtn').toggleClass('flipped');
+  });
+
+  $('.cardListHeader .expandBtn').on('click', function() {
+    $('.cardListSection').toggleClass('hidden');
+    $('.cardListHeader .expandBtn').toggleClass('flipped');
+  });
+
+  $('.minFeatureHeader .expandBtn').on('click', function() {
+    $('.minFeatureSection').toggleClass('hidden');
+    $('.minFeatureHeader .expandBtn').toggleClass('flipped');
+  });
+
+  $('.personalHeader .expandBtn').on('click', function() {
+    $('.personalSection').toggleClass('hidden');
+    $('.personalHeader .expandBtn').toggleClass('flipped');
+  });
+
+  $('.basicHeader .expandBtn').on('click', function() {
+    $('.basicSection').toggleClass('hidden');
+    $('.basicHeader .expandBtn').toggleClass('flipped');
+  });
+
 });
